eda73950ac12ff2142a660ce8386a2b368ed8136
Rudrasingh-hub <suhagrudrasingh@gmail.com>
2026-05-12 12:04:54 +0530

    Update app.js

--- app.js
+++ app.js
@@ -29,3 +29,41 @@ function addEntry() {
   document.getElementById("study").value = "";
   document.getElementById("sleep").value = "";
 }
+let count = 0;
+let totalStudy = 0;
+
+function addEntry() {
+  const studyInput = document.getElementById("study");
+  const sleepInput = document.getElementById("sleep");
+
+  const study = Number(studyInput.value);
+  const sleep = Number(sleepInput.value);
+
+  // validation
+  if (!study || !sleep) {
+    alert("Enter valid values!");
+    return;
+  }
+
+  // create entry
+  const li = document.createElement("li");
+
+  const today = new Date().toLocaleDateString();
+
+  li.innerHTML = `📅 ${today} | 📚 ${study}h | 😴 ${sleep}h`;
+
+  document.getElementById("list").appendChild(li);
+
+  // update stats
+  count++;
+  totalStudy += study;
+
+  document.getElementById("total").innerText = count;
+
+  document.getElementById("avg").innerText =
+    (totalStudy / count).toFixed(1) + "h";
+
+  // clear inputs
+  studyInput.value = "";
+  sleepInput.value = "";
+}
